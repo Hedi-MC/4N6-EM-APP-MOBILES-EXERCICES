69a5f5a07726619e13b7da3f834833b57e67bab3
Hedi Melo Cherni <2030719@cegepmontpetit.ca>
2024-05-07 12:28:48 -0400

    Formatif final pt1

---
+++ Formatif Examen Final/app/src/main/java/org/melocherni/ExamenFinal/MainActivity.java
@@ -0,0 +1,16 @@
+package org.melocherni.ExamenFinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle(getString(R.string.good_good_good));
+    }
+}
